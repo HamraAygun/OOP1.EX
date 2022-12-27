c5047f5bb7b86574d67f1a2fc1f2f3a816a8c78c
khas <khas@MYLABPC-D01>
2022-12-27 13:05:58 +0300

    Add project files.

---
+++ OOP1.EX/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Students students = new Students();
+
+        students.Name = "Hamra";
+        students.Surname = "Aygün";
+        students.Age = 27;
+        students.No = 1;
+        students.Gender = "Famale";
+
+        Console.WriteLine("Öğrencinin Adı: " + students.Name + " - " + "Öğrencinin Soyadı: " + students.Surname + " - " + "Öğrencinin Yaşı: " + students.Age + " - "+ "Öğrencinin Numarası: " + " - " + "Öğrencinin Cinsiyeti: " + students.Gender);
+
+        Console.ReadKey();
+    }
+    
+    public class Students
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int No { get; set; }
+        public string Gender { get; set; }
+    }
+
+
+    
+   
+}
